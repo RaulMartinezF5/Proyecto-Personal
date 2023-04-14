41cfbdf559b36f786481137bde8fee3cc990179c
RaulMartinezF5 <raulmartinezalvarez1980@gmail.com>
2023-04-14 11:41:48 +0200

    errores imagenes solucionados

--- src/main/frontend/src/views/Homeview.vue
+++ src/main/frontend/src/views/Homeview.vue
@@ -1,8 +1,12 @@
 <script setup>
+import Header from '../components/Header.vue';
+import Home from '../components/Home.vue';
+import Footer from '../components/Footer.vue';
 </script>
 
 <template>
-  <main>
+     <Header />
+    <!-- <Home />
+    <Footer /> -->
 
-  </main>
 </template>
